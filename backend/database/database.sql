515cbc948fa9b335d5ee9389a6d4da2c2eac998f
Gabriel Pescaroli <ShaickMiles@gmail.com>
2023-03-29 15:48:38 -0300

    db

--- backend/database/database.sql
+++ backend/database/database.sql
@@ -1,1 +1,44 @@
-CREATE TABLE 
+
+/*
+types:
+1 - cliente
+2 - garçon
+3 - cozinheiro
+4 - administrador
+*/
+CREATE TABLE UserConfig(
+    uid varchar(255) primary key,
+    type integer not null
+);
+
+CREATE TABLE Mesa(
+    id serial primary key,
+    numero integer not null,
+    numeroMaxPessoas integer not null,
+    ocupado boolean not null default false
+);
+
+CREATE TABLE Categoria(
+    id serial primary key,
+    nome varchar(255) not null
+);
+
+CREATE TABLE Produto(
+    id serial primary key,
+    nome varchar(255) not null,
+    idCategoria integer foreign key Categoria,
+    valor numeric(10,2) not null
+);
+
+CREATE TABLE Pedido(
+    id serial primary key,
+    idMesa integer foreign key Mesa,
+    status integer
+);
+
+CREATE TABLE PedidoItem(
+    id serial primary key,
+    idPedido integer foreign key Pedido,
+    idProduto integer foreign key Produto,
+    valor numeric(10,2) not null
+);
