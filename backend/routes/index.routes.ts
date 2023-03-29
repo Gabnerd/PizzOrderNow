302b103e7bc82a73c9484b0bd9d57718f98ea00f
Gabriel Pescaroli <ShaickMiles@gmail.com>
2023-03-29 16:18:57 -0300

    routers setup

--- backend/routes/index.routes.ts
+++ backend/routes/index.routes.ts
@@ -1,9 +1,14 @@
 import {Request, Response, Router} from 'express'
+import mesaRouter from './mesa.routes'
+import pedidoRouter from './pedido.routes'
 
 const router = Router();
 
 router.get("/", (req: Request ,res: Response)=>{
     res.send("<h1>Pizza Order API</h1>");
 });
+
+router.use(mesaRouter);
+router.use(pedidoRouter);
 
 export default router;
